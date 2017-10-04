1dcfb006ca52fc07a8e5b54dc95f82f99820df31
Kimberly Bordon <kbordon@gmail.com>
2017-10-03 23:00:49 -0700

    Add another scroll event, and create function to append boxes based on day.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,51 @@
 $(document).ready(function(){
   // $('.parallax').parallax();
+  var prompts = ["swift", "divided", "poison", "underwater", "long", "sword",
+                "shy", "crooked", "screech", "gigantic", "run", "shattered",
+                "teeming", "fierce", "mystery", "fat", "graceful", "filthy",
+                "cloud", "deep", "furious", "trail", "juicy", "blind", "ship",
+                "squeak", "climb", "fall", "united", "found", "mask"];
 
+  var d = new Date();
+  var n = d.getDate();
 
-  var scrollHeight = $(document).height();
-  var windowWidth = ($(window).width());
-  var windowHeight = $(window).height()/1.25;
+  for (var index = 0; index < n; index++) {
+    if ( index % 2 === 0) {
+      $("#even-boxes").append("<div class='prompt-box'>" +
+                                   + (index + 1) + ". " + prompts[index] +
+                                "</div>");
+    } else {
+      $("#odd-boxes").append("<div class='prompt-box'>" +
+                                 + (index + 1) + ". " + prompts[index] +
+                              "</div>");
+    }
+  }
+  prompts.forEach(function(prompt){
+    $("#prompt-boxes").append("<div class='prompt-box'>" +
+                                 + 1 + ". " + prompt +
+                              "</div>");
+  });
+
+  $(window).scroll(function() {
+    $(".prompt-box").each(function() {
+      var hT = $(this).offset().top;
+      var hH = $(this).outerHeight();
+      var wH = $(window).height()/2;
+      var wS = $(window).scrollTop();
+      if (wS > (hT+hH-wH)){
+        $(this).animate({
+          opacity: 1,
+        }, 1200, function() {
+        });
+      }
+    });
+ });
+
   $(window).scroll(function(event){
+    // var footerHeight = $("footer").outerHeight(true);
+    var scrollHeight = $(document).height();
+    var windowWidth = ($(window).width());
+    var windowHeight = $(window).height()/1.25;
     // $("#penimg").animate({opacity: 1}, 800, function(){});
     var scrollTop = $(window).scrollTop();
     var scrollAmount = (scrollTop / (scrollHeight - windowHeight));
